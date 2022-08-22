f12587fa3cd58c891a4dafa25cab2162efa725e0
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-08-22 13:48:16 -0300

    Criação de função de virar cartas

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let vetorImagem = [
     'tripletsparrot',
     'unicornparrot'
 ];
+let clickBooleano = false;
+let primeiraCarta;
+let cliques = 0;
+let cliquesdois = 0;
+let comparador = 0;
 
 while(tamanho < 4 || tamanho > 14 || tamanho%2 != 0) {
     tamanho = prompt('Quantidade inválida, insira uma quantidade válida de cartas');
@@ -39,6 +44,47 @@ function adicionaCartas() {
         carta.classList.add('carta');
         container.appendChild(carta);
     }    
+}
+
+function viraCarta(carta) {
+    cliquesdois++; 
+
+    if(cliquesdois > 2) {
+        return;
+    } // não mexer
+
+    carta.classList.add('virar');
+    carta.removeAttribute('onClick');
+    cliques++; 
+
+    if(clickBooleano) {
+        if(primeiraCarta.children[1].getAttribute('src') === carta.children[1].getAttribute('src')) {
+            carta.removeAttribute('onClick');
+            primeiraCarta.removeAttribute('onClick');
+            comparador++;
+            if(comparador === parseInt(tamanho/2)) {
+                setTimeout(() => {
+                    alert(`Você ganhou em ${cliques} jogadas! `);
+                }, 1000);
+            }
+
+            cliquesdois = 0;
+        } else {
+            setTimeout(() => {
+                carta.classList.remove('virar');
+                primeiraCarta.classList.remove('virar');
+                cliquesdois = 0;
+                primeiraCarta.setAttribute('onClick', 'viraCarta(this)');
+                carta.setAttribute('onClick', 'viraCarta(this)');                
+            }, 1000);
+        }
+
+        clickBooleano = false;
+    } else {
+        clickBooleano = true;
+        primeiraCarta =  carta;
+        console.log(primeiraCarta);
+    }
 }
 
 adicionaCartas();
